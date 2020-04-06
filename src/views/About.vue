e0674d23e59701bd277b6eba11d72084fce6d645
thibautguedou3 <thibautgu@bam.tech>
2020-04-06 11:35:16 +0200

    :sparkles: update home and about page with external links

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <a href="https://www.eurosport.fr" target="_blank">
+      Mon autre super lien externe
+    </a>
   </div>
 </template>
